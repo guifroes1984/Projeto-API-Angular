002e69446993b0e680c306aa021f290dffa1ecdb
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-25 13:08:24 -0300

    Implementando paginacao no servico de lancamentos.

--- src/app/lancamentos/lancamento.service.ts
+++ src/app/lancamentos/lancamento.service.ts
@@ -4,17 +4,19 @@ import { Injectable, OnInit } from '@angular/core';
 import 'rxjs/add/operator/toPromise';
 import * as moment from 'moment';
 
-export interface LancamentoFiltro {
+export class LancamentoFiltro {
   descricao: string;
   dataVencimentoInicio: Date;
   dataVencimentoFim: Date;
+  pagina = 0;
+  itensPorPagina = 5;
 }
 
 @Injectable()
 export class LancamentoService {
 
   lancamentosUrl = 'http://localhost:8080/lancamentos';
-  
+
   constructor(private http: Http) { }
 
 
@@ -24,24 +26,37 @@ export class LancamentoService {
 
     headers.append('Authorization', 'Basic YWRtaW5AYWRtaW4uY29tOmFkbWlu');
 
+    params.set('page', filtro.pagina.toString());
+    params.set('size', filtro.itensPorPagina.toString());
+
     if (filtro.descricao) {
       params.set('descricao', filtro.descricao);
     }
 
     if (filtro.dataVencimentoInicio) {
-      params.set('dataVencimentoDe', 
+      params.set('dataVencimentoDe',
         moment(filtro.dataVencimentoInicio).format('YYYY-MM-DD'))
     }
 
     if (filtro.dataVencimentoFim) {
-      params.set('dataVencimentoAte', 
+      params.set('dataVencimentoAte',
         moment(filtro.dataVencimentoFim).format('YYYY-MM-DD'))
     }
 
-    return this.http.get(`${this.lancamentosUrl}?resumo`, 
+    return this.http.get(`${this.lancamentosUrl}?resumo`,
         { headers, search: params })
       .toPromise()
-      .then(response => response.json().content)
+      .then(response => {
+        const responseJson = response.json();
+        const lancamentos = responseJson.content;
+
+        const resultado = {
+          lancamentos, 
+          total: responseJson.totalElements
+        };
+
+        return resultado;
+      })
   }
 
 }
