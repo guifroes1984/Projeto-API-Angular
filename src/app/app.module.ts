d9ab05bd071ab429024cb3b3a60c3e934b62390a
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-28 15:22:06 -0300

    Configurando rotas na aplicacao.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
+import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
+import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
+
+const routes: Routes = [
+  { path: 'lancamentos', component: LancamentosPesquisaComponent }, 
+  { path: 'lancamentos/novo', component: LancamentoCadastroComponent }, 
+  { path: 'pessoas', component: PessoasPesquisaComponent } 
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +26,8 @@ import { LancamentosModule } from './lancamentos/lancamentos.module';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    HttpModule,
+    HttpModule, 
+    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
