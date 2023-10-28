64bc3a8d2b5d72f37e00b9037b35a90ec3475344
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-27 23:19:54 -0300

    Implementando o listando as pessoas cadastradas no dropdown.

--- src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
+++ src/app/lancamentos/lancamento-cadastro/lancamento-cadastro.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { ErrorHandleService } from 'app/core/error-handle.service';
 import { CategoriaService } from '../../categorias/categoria.service';
+import { PessoaService } from '../../pessoas/pessoas.service';
+
 
 
 @Component({
@@ -17,31 +19,33 @@ export class LancamentoCadastroComponent implements OnInit {
     { label: 'Despesa', value: 'DESPESA' },
   ];
 
-  categorias = [
-    { label: 'Alimentação', value: 1 },
-    { label: 'Transporte', value: 2 },
-  ];
-
-  pessoas = [
-    { label: 'João da Silva', value: 1 },
-    { label: 'Sebastião Souza', value: 2 },
-    { label: 'Maria Abadia', value: 3 },
-  ];
+  categorias = [];
+  pessoas = [];
 
   constructor(
     private categoriaService: CategoriaService, 
+    private pessoaService: PessoaService, 
     private errorHandle: ErrorHandleService
     
     ) { }
 
   ngOnInit() {
     this.carregarCategorias();
+    this.carregarPessoas();
   }
 
   carregarCategorias() {
     return this.categoriaService.listarTodas()
       .then(categorias => {
         this.categorias = categorias.map(c => ({ label: c.nome, value: c.codigo }));
+      })
+      .catch(error => this.errorHandle.handle(error));
+  }
+
+  carregarPessoas() {
+    this.pessoaService.listarTodas() 
+      .then(pessoas => {
+        this.pessoas = pessoas.map(p => ({ label: p.nome, value:p.codigo }))
       })
       .catch(error => this.errorHandle.handle(error));
   }
