fee559899c77aa016826b9e66052587914152c5f
guifroes1984 <guilhermefroes26@gmail.com>
2023-10-30 19:23:09 -0300

    Refatorando as rotas para usar routing module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,26 +3,14 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
-import { LancamentosPesquisaComponent } from './lancamentos/lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoCadastroComponent } from './lancamentos/lancamento-cadastro/lancamento-cadastro.component';
-import { PessoasPesquisaComponent } from './pessoas/pessoas-pesquisa/pessoas-pesquisa.component';
-import { PaginaNaoEncontradaComponent } from './core/pagina-nao-encontrada.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const routes: Routes = [
-  { path: '', redirectTo: 'lancamentos', pathMatch: 'full' },
-  { path: 'lancamentos', component: LancamentosPesquisaComponent }, 
-  { path: 'lancamentos/novo', component: LancamentoCadastroComponent }, 
-  { path: 'lancamentos/:codigo', component: LancamentoCadastroComponent }, 
-  { path: 'pessoas', component: PessoasPesquisaComponent }, 
-  { path: 'pagina-nao-encontrada', component: PaginaNaoEncontradaComponent }, 
-  { path: '**', redirectTo: 'pagina-nao-encontrada' }
-];
+
 
 @NgModule({
   declarations: [
@@ -32,11 +20,11 @@ const routes: Routes = [
     BrowserModule,
     BrowserAnimationsModule,
     HttpModule, 
-    RouterModule.forRoot(routes),
 
     CoreModule,
     LancamentosModule,
-    PessoasModule
+    PessoasModule, 
+    AppRoutingModule
   ],
   providers: [
 
