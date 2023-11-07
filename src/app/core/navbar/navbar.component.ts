4bb329c23a2cde5c73b76808f08ecdae7a7df2bb
guifroes1984 <guilhermefroes26@gmail.com>
2023-11-07 19:17:25 -0300

    Implementando o logout.

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+
+import { LogoutService } from 'app/seguranca/logout.service';
+import { ErrorHandleService } from '../error-handle.service';
 import { AuthService } from 'app/seguranca/auth.service';
 
 @Component({
@@ -8,13 +12,22 @@ import { AuthService } from 'app/seguranca/auth.service';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private auth: AuthService) { }
+  constructor(
+    private auth: AuthService,
+    private logoutService: LogoutService,
+    private errorHadle: ErrorHandleService,
+    private router: Router
+    ) { }
 
   ngOnInit() {
   }
 
-  criarNovoAccessToken() {
-    this.auth.obterNovoAccesstoken();
+  logout() {
+    this.logoutService.logout()
+      .then(() => {
+        this.router.navigate(['/login']);
+      })
+      .catch(erro => this.errorHadle.handle(erro));
   }
 
 }
